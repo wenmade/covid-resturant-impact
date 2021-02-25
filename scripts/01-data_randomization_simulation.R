977867d6378c6e24e1e8ac210d0638fad94a762f
Wen Ma <wenmadesign@gmail.com>
2021-02-24 22:07:51 -0500

    update data and dataset

--- scripts/01-data_randomization_simulation.R
+++ scripts/01-data_randomization_simulation.R
@@ -15,7 +15,6 @@ dinesafeCSV <- dinesafeCSV[dinesafeCSV$TYPE == 'Restaurant',]
 totalobs <- nrow(dinesafeCSV)
 
 
-
 #### Establish some verctors ####
 
 top_cuisine_list <- c("Chinese", "Mexican", "Italian", "Japanese", "Greek", "Korean", "Thai", "Vietnamese", "American",
@@ -25,7 +24,7 @@ top_cuisine_list <- c("Chinese", "Mexican", "Italian", "Japanese", "Greek", "Kor
 business_op_list <- c("Under 1 year", "1-5 years", "6-10 years", "11-20 years", "Over 20 years");
 business_owner_list <- c("Under 1 year", "1-5 years", "6-10 years", "11-20 years", "Over 20 years");
 price_point_target <- c("Low-end", "Average", "High-end");
-
+rating_list <- c("1", "2", "3", "4", "5");
 
 #### Raw Survey Data Radomization ####
 
@@ -86,19 +85,22 @@ simulated_resturants <-
                                 size=totalobs,
                                 replace=TRUE,
                                 prob=c(0.4, 0.4, 0.2)),
-    Income_052019_102019 = runif(totalobs, min=750000, max=5000000) %>% as.integer(), #set bottom bar 750000
-    Income_052020_102020 = runif(totalobs, min=750000, max=1000000) %>% as.integer(),
-    Income_052021_102021 = runif(totalobs, min=750000, max=5000000) %>% as.integer(),
-    Profit_052019_102019 = runif(totalobs, min=5000, max=40000) %>% as.integer(), #set bottom bar 5000
-    Profit_052020_102020 = runif(totalobs, min=5000, max=25000) %>% as.integer(),
-    Profit_052021_102021 = runif(totalobs, min=5000, max=40000) %>% as.integer(),
+    Income_052019_062019 = runif(totalobs, min=120000, max=800000) %>% as.integer(), #Income_052019_062019
+    Income_052020_062020 = runif(totalobs, min=60000, max=160000) %>% as.integer(),
+    Income_052021_062021 = runif(totalobs, min=120000, max=800000) %>% as.integer(),
+    Profit_052019_062019 = runif(totalobs, min=3600, max=24000) %>% as.integer(), #data filling using 0.03 from income
+    Profit_052020_062020 = runif(totalobs, min=1800, max=4800) %>% as.integer(), 
+    Profit_052021_062021 = runif(totalobs, min=3600, max=24000) %>% as.integer(),
 
     Selected_Open = sample(x = c ("Y", "N"),
                            size=totalobs,
                            replace=TRUE,
                            prob=c(0.5, 0.5)),
 
-    Helpfulness = runif(totalobs, min=1, max=5) %>% as.integer(),
+    Helpfulness = sample(x = rating_list, 
+                         size = totalobs,
+                         replace=TRUE,
+                         prob=c(0.2, 0.1, 0.15, 0.2, 0.35)),
     
     Long = dinesafeCSV$LONGITUDE, #from dinesafe
     Lat = dinesafeCSV$LATITUDE #from dinesafe
@@ -107,108 +109,99 @@ simulated_resturants <-
 
 #Some data simulation to make it more realistic - all assumptions
 
-#1. If resturant is selected to reopen, we allow assumptions to have profit from may-oct 2021 of 10000 - 40000, 
-#otherwise 0 - 25000, staff number for not reopened resturants stay the same as 2020, 1-20.
+#1. If resturant is selected to reopen, we allow assumptions to have profit from may-jun 2021,
+#staff number for not reopened resturants stay the same as 2020, 1-20.
 indexSY <- which(simulated_resturants$Selected_Open == "Y")
-simulated_resturants$Profit_052021_102021[indexSY] <- runif(length(indexSY), min=10000, max=40000) %>% as.integer()
+simulated_resturants$Profit_052021_062021[indexSY] <- runif(length(indexSY), min=3600, max=24000) %>% as.integer()
 indexSN <- which(simulated_resturants$Selected_Open == "N")
-simulated_resturants$Profit_052021_102021[indexSN] <- runif(length(indexSN), min=0, max=25000) %>% as.integer()
+simulated_resturants$Profit_052021_062021[indexSN] <- runif(length(indexSN), min=1800, max=4800) %>% as.integer()
 simulated_resturants$Number_Of_Staff_During_Reopen[indexSN] <- runif(length(indexSN), min=1, max=20) %>% as.integer()
 
-#2. If resturant is highend, we allow Income to be 3000000 - 5000000, if average, we allow income to be 1500000 - 3500000, if lowend, we allow income to be 750000 - 2000000.
+#2. If resturant is highend, we allow Income to be ---, if average, we allow income to be ---, if lowend, we allow income to be ----.
 # For every raise in pricepoint, we cutdown half for the min, and 1.5million for the max. (2019)
 indexIL <- which(simulated_resturants$Price_Point_Target == "Low-end")
-simulated_resturants$Income_052019_102019[indexIL] <- runif(length(indexIL), min=750000, max=2000000) %>% as.integer()
+simulated_resturants$Income_052019_062019[indexIL] <- runif(length(indexIL), min=120000, max=276667) %>% as.integer()
+simulated_resturants$Income_052021_062021[indexIL] <- runif(length(indexIL), min=120000, max=276667) %>% as.integer()
 indexIA <- which(simulated_resturants$Price_Point_Target == "Average")
-simulated_resturants$Income_052019_102019[indexIA] <- runif(length(indexIA), min=1500000, max=3500000) %>% as.integer()
+simulated_resturants$Income_052019_062019[indexIA] <- runif(length(indexIA), min=176667, max=523333) %>% as.integer()
+simulated_resturants$Income_052021_062021[indexIA] <- runif(length(indexIA), min=176667, max=523333) %>% as.integer()
 indexIH <- which(simulated_resturants$Price_Point_Target == "High-end")
-simulated_resturants$Income_052019_102019[indexIH] <- runif(length(indexIH), min=3000000, max=5000000) %>% as.integer()
+simulated_resturants$Income_052019_062019[indexIH] <- runif(length(indexIH), min=403333, max=800000) %>% as.integer()
+simulated_resturants$Income_052021_062021[indexIH] <- runif(length(indexIH), min=403333, max=800000) %>% as.integer()
 
-#cap resturants incomes assuming no resturant will make more profit/income than 2019. 
-simulated_resturants$Income_052021_102021[indexIL] <- runif(length(indexIL), min=750000, max=2000000) %>% as.integer()
-simulated_resturants$Income_052021_102021[indexIA] <- runif(length(indexIA), min=1500000, max=3500000) %>% as.integer()
-simulated_resturants$Income_052021_102021[indexIH] <- runif(length(indexIH), min=3000000, max=5000000) %>% as.integer()
+#cap resturants incomes assuming no resturant will make more profit/income than 2019. with random of 90% - 50% income
+reduction2021 <- runif(totalobs, min=0.5, max=0.9)
+simulated_resturants$Income_052021_062021[indexIL] <- floor(simulated_resturants$Income_052021_062021[indexIL]*reduction2021)
+simulated_resturants$Income_052021_062021[indexIA] <- floor(simulated_resturants$Income_052021_062021[indexIA]*reduction2021)
+simulated_resturants$Income_052021_062021[indexIH] <- floor(simulated_resturants$Income_052021_062021[indexIH]*reduction2021)
 
 #2a. We assume that all resturants will receive lower income during covid, therefore, the max income from 052020 - 102020 will be based on the 052019-102019 minus a random 
-# percentage from 30% - 75% 
+# percentage from 30% - 70% 
 reduction <- runif(totalobs, min=0.3, max=0.7)
-calculatedmax = floor(simulated_resturants$Income_052019_102019*reduction)
-simulated_resturants$Income_052020_102020 <- runif(totalobs, min=0, max=calculatedmax) %>% as.integer()
+calculatedmax = floor(simulated_resturants$Income_052019_062019*reduction)
+simulated_resturants$Income_052020_062020 <- runif(totalobs, min=0, max=calculatedmax) %>% as.integer()
 
-#indexOF <- which(simulated_resturants$Income_052020_102020 > 1000000)
-#simulated_resturants$Income_052020_102020[indexOF] <- 1000000
+#2aa. If the resturant is not selected to open, 2021 income and profit goes down by 30% - 70% based on their 2019 data. 
+# Profit is capped
+indexIncProf <- which(simulated_resturants$Selected_Open == "N")
+reductionIncProf <- runif(length(indexIncProf), min=0.85, max=1.15)
+simulated_resturants$Income_052021_062021[indexIncProf] <- floor(simulated_resturants$Income_052020_062020[indexIncProf]*reductionIncProf)
+
 
 #2b. If resturant is highend, we allow Profit to be 20000 - 40000, if average, we allow profit to be 10000 - 25000, if low-end, we allow profit to be 0-10000.
 # For every raise in pricepoint, we cutdown half for the min, and 15000 for the max. (2019)
-indexPL <- which(simulated_resturants$Price_Point_Target == "Low-end")
-simulated_resturants$Profit_052019_102019[indexPL] <- runif(length(indexPL), min=5000, max=10000) %>% as.integer()
-indexPA <- which(simulated_resturants$Price_Point_Target == "Average")
-simulated_resturants$Profit_052019_102019[indexPA] <- runif(length(indexPA), min=10000, max=25000) %>% as.integer()
-indexPH <- which(simulated_resturants$Price_Point_Target == "High-end")
-simulated_resturants$Profit_052019_102019[indexPH] <- runif(length(indexPH), min=20000, max=40000) %>% as.integer()
+profitpercentage <- runif(totalobs, min=0.02, max=0.045)
 
-#cap resturants profits assuming no resturant will make more profit/income than 2019. 
-simulated_resturants$Profit_052021_102021[indexPL] <- runif(length(indexPL), min=5000, max=10000) %>% as.integer()
-simulated_resturants$Profit_052021_102021[indexPA] <- runif(length(indexPA), min=10000, max=25000) %>% as.integer()
-simulated_resturants$Profit_052021_102021[indexPH] <- runif(length(indexPH), min=20000, max=40000) %>% as.integer()
+indexPL <- which(simulated_resturants$Price_Point_Target == "Low-end")
+simulated_resturants$Profit_052019_062019[indexPL] <- floor(simulated_resturants$Income_052019_062019[indexPL]*profitpercentage)
+simulated_resturants$Profit_052021_062021[indexPL] <- floor(simulated_resturants$Income_052021_062021[indexPL]*profitpercentage)
+indexPA <- which(simulated_resturants$Price_Point_Target == "Average")
+simulated_resturants$Profit_052019_062019[indexPA] <- floor(simulated_resturants$Income_052019_062019[indexPA]*profitpercentage)
+simulated_resturants$Profit_052021_062021[indexPA] <- floor(simulated_resturants$Income_052021_062021[indexPA]*profitpercentage)
+indexPH <- which(simulated_resturants$Price_Point_Target == "High-end")
+simulated_resturants$Profit_052019_062019[indexPH] <- floor(simulated_resturants$Income_052019_062019[indexPH]*profitpercentage)
+simulated_resturants$Profit_052021_062021[indexPH] <- floor(simulated_resturants$Income_052021_062021[indexPH]*profitpercentage)
+
 
 
 #2c. We assume that all resturants will receive lower profit during covid, therefore, the max profit from 052020 - 102020 will be based on the 052019-102019 minus a random 
-# percentage from 30% - 75% with cap of 25000
-reductionP <- runif(totalobs, min=0.3, max=0.75)
-calculatedmaxP <- floor(simulated_resturants$Profit_052019_102019 - simulated_resturants$Profit_052019_102019*reductionP)
-simulated_resturants$Profit_052020_102020 <- runif(totalobs, min=0, max=calculatedmaxP) %>% as.integer()
-indexOFP <- which(simulated_resturants$Profit_052020_102020 > 25000)
-simulated_resturants$Profit_052020_102020[indexOFP] <- 25000
+# percentage from 30% - 70% with cap of 25000
+reductionP <- runif(totalobs, min=0.3, max=0.7)
+calculatedmaxP <- floor(simulated_resturants$Profit_052019_062019 - simulated_resturants$Profit_052019_062019*reductionP)
+simulated_resturants$Profit_052020_062020 <- runif(totalobs, min=0, max=calculatedmaxP) %>% as.integer()
 
-#2ca. If the resturant is not selected to open, income and profit goes down by 30% - 75% based on their 2019 data. 
-# Profit is capped
-indexIncProf <- which(simulated_resturants$Selected_Open == "N")
-reductionIncProf <- runif(length(indexIncProf), min=0.3, max=0.75)
-simulated_resturants$Profit_052021_102021[indexIncProf] <- floor(simulated_resturants$Profit_052019_102019[indexIncProf] - 
-                                                                   simulated_resturants$Profit_052019_102019[indexIncProf]*reductionIncProf)
 
-indexCOFP <- which(simulated_resturants$Profit_052021_102021 > 25000)
-simulated_resturants$Profit_052021_102021[indexCOFP] <- 25000
 
-simulated_resturants$Income_052021_102021[indexIncProf] <- floor(simulated_resturants$Income_052021_102021*reductionIncProf)
-
-#2d. If the resturance is not selected to open and not offering curb-side pick up or delivery, we assume the profit and income would be 0.(2020 and 2021)
+#2d. If the resturant is not selected to open and not offering curb-side pick up or delivery, 
+#we assume the profit and income would be 0.(2020 and 2021)
 indexNCND <- which(simulated_resturants$Selected_Open == "N" && 
                      simulated_resturants$Curbside_Pickup == "N" && 
                      simulated_resturants$Delivery == "N")
-simulated_resturants$Profit_052021_102021[indexNCND] <- 0
-simulated_resturants$Income_052021_102021[indexNCND] <- 0
-simulated_resturants$Profit_052020_102020[indexNCND] <- 0
-simulated_resturants$Income_052020_102020[indexNCND] <- 0
+simulated_resturants$Profit_052021_062021[indexNCND] <- 0
+simulated_resturants$Income_052021_062021[indexNCND] <- 0
+simulated_resturants$Profit_052020_062020[indexNCND] <- 0
+simulated_resturants$Income_052020_062020[indexNCND] <- 0
 simulated_resturants$Total_Hours_Per_Week_During_Reopen[indexNCND] <- 0
 
 #3. If the resturant offers dine-in service, 
 #we make assumption to reduce the income and profit by 30% - 50% of its current number (2021).
-indexDIS <- which(simulated_resturants$Dine_In_Service == "Y")
-reductionDIS <- runif(length(indexDIS), min=0.3, max=0.5)
-simulated_resturants$Income_052021_102021[indexDIS] <- floor(simulated_resturants$Income_052021_102021[indexDIS] - 
-  simulated_resturants$Income_052021_102021[indexDIS]*reductionDIS)
-simulated_resturants$Profit_052021_102021[indexDIS] <- floor(simulated_resturants$Profit_052021_102021[indexDIS] - 
-  simulated_resturants$Profit_052021_102021[indexDIS]*reductionDIS)
+indexDIS <- which(simulated_resturants$Dine_In_Service == "Y" && simulated_resturants$Selected_Open == "N")
+reductionDIS <- runif(length(indexDIS), min=0.5, max=0.7)
+simulated_resturants$Income_052021_062021[indexDIS] <- floor(simulated_resturants$Income_052021_062021[indexDIS]*reductionDIS)
+simulated_resturants$Profit_052021_062021[indexDIS] <- floor(simulated_resturants$Profit_052021_062021[indexDIS]*reductionDIS)
 
 
 #4. If resturant is selected to open, helpfulness is under assumption of 1-5, otherwise, fill in 0
-indexHY <- which(simulated_resturants$Selected_Open == "Y")
-simulated_resturants$Helpfulness[indexHY] <- runif(length(indexHY), min=1, max=5) %>% as.integer()
 indexHN <- which(simulated_resturants$Selected_Open == "N")
 simulated_resturants$Helpfulness[indexHN] <- 0
 
 
 #5. If the resturant is lease, then profit goes down 20% - 40%. (all years)
 indexLe <- which(simulated_resturants$Own_Lease == "Lease" || simulated_resturants$Own_Lease == "Lease, want to purchase")
-reductionLe <- runif(length(indexLe), min=0.2, max=0.4)
-simulated_resturants$Profit_052019_102019[indexLe] <- floor(simulated_resturants$Profit_052019_102019[indexLe] - 
-                                                      simulated_resturants$Profit_052019_102019[indexLe]*reductionLe)
-simulated_resturants$Profit_052020_102020[indexLe] <- floor(simulated_resturants$Profit_052020_102020[indexLe] - 
-  simulated_resturants$Profit_052020_102020[indexLe]*reductionLe)
-simulated_resturants$Profit_052021_102021[indexLe] <- floor(simulated_resturants$Profit_052021_102021[indexLe] - 
-  simulated_resturants$Profit_052021_102021[indexLe]*reductionLe)
+reductionLe <- runif(length(indexLe), min=0.6, max=0.8)
+simulated_resturants$Profit_052019_062019[indexLe] <- floor(simulated_resturants$Profit_052019_062019[indexLe]*reductionLe)
+simulated_resturants$Profit_052020_062020[indexLe] <- floor(simulated_resturants$Profit_052020_062020[indexLe]*reductionLe)
+simulated_resturants$Profit_052021_062021[indexLe] <- floor(simulated_resturants$Profit_052021_062021[indexLe]*reductionLe)
 
 #5. Randomly fill in NA value for website to make it look real(er)
 index <- which(simulated_resturants$Website %in% sample(simulated_resturants$Website, 456))
@@ -221,12 +214,12 @@ indexDineBug <- which(simulated_resturants$Dine_In_Service == "N" &&
 simulated_resturants$Delivery[indexDineBug] <- "Y"
 
 #6. Randomization bug: if income is lower than profit of the year, then set income to profit
-indexProfIncBug19 <- which(simulated_resturants$Income_052019_102019 < simulated_resturants$Profit_052019_102019)
-simulated_resturants$Income_052019_102019[indexProfIncBug19] <- simulated_resturants$Profit_052019_102019[indexProfIncBug19]
-indexProfIncBug20 <- which(simulated_resturants$Income_052020_102020 < simulated_resturants$Profit_052020_102020)
-simulated_resturants$Income_052020_102020[indexProfIncBug20] <- simulated_resturants$Profit_052020_102020[indexProfIncBug20]
-indexProfIncBug21 <- which(simulated_resturants$Income_052021_102021 < simulated_resturants$Profit_052021_102021)
-simulated_resturants$Income_052021_102021[indexProfIncBug21] <- simulated_resturants$Profit_052021_102021[indexProfIncBug21]
+indexProfIncBug19 <- which(simulated_resturants$Income_052019_062019 < simulated_resturants$Profit_052019_062019)
+simulated_resturants$Income_052019_062019[indexProfIncBug19] <- simulated_resturants$Profit_052019_062019[indexProfIncBug19]
+indexProfIncBug20 <- which(simulated_resturants$Income_052020_062020 < simulated_resturants$Profit_052020_062020)
+simulated_resturants$Income_052020_062020[indexProfIncBug20] <- simulated_resturants$Profit_052020_062020[indexProfIncBug20]
+indexProfIncBug21 <- which(simulated_resturants$Income_052021_062021 < simulated_resturants$Profit_052021_062021)
+simulated_resturants$Income_052021_062021[indexProfIncBug21] <- simulated_resturants$Profit_052021_062021[indexProfIncBug21]
 
 # Write final csv for cleaning and preparation
 write_csv(simulated_resturants, "dataset/raw_data_all_resturants.csv")
--- scripts/04_Sample_Resturant_Analysis.R
+++ scripts/04_Sample_Resturant_Analysis.R
@@ -3,20 +3,20 @@ sample_resturants = read.csv('dataset/sample_resturants.csv', stringsAsFactors =
 
 #histogram over 3 years profit
 ggplot(data = sample_resturants) +
-  geom_histogram(aes(x = Profit_052019_102019, y=(..count..)/sum(..count..)), 
+  geom_histogram(aes(x = Profit_052019_062019, y=(..count..)/sum(..count..)), 
                  alpha=0.3, fill ="red",position="identity") +
-  geom_histogram(aes(x = Profit_052020_102020, y=(..count..)/sum(..count..)), 
+  geom_histogram(aes(x = Profit_052020_062020, y=(..count..)/sum(..count..)), 
                  alpha=0.3,, fill ="green",position="identity") +
-  geom_histogram(aes(x = Profit_052021_102021, y=(..count..)/sum(..count..)), 
+  geom_histogram(aes(x = Profit_052021_062021, y=(..count..)/sum(..count..)), 
                  alpha=0.7,, fill ="blue",position="identity")
 
 #histogram over 3 years income
 ggplot(data = sample_resturants) +
-  geom_histogram(aes(x = Income_052019_102019, y=(..count..)/sum(..count..)), 
+  geom_histogram(aes(x = Income_052019_062019, y=(..count..)/sum(..count..)), 
                  alpha=0.3, fill ="red",position="identity") +
-  geom_histogram(aes(x = Income_052020_102020, y=(..count..)/sum(..count..)), 
+  geom_histogram(aes(x = Income_052020_062020, y=(..count..)/sum(..count..)), 
                  alpha=0.3,, fill ="green",position="identity") +
-  geom_histogram(aes(x = Income_052021_102021, y=(..count..)/sum(..count..)), 
+  geom_histogram(aes(x = Income_052021_062021, y=(..count..)/sum(..count..)), 
                  alpha=0.7,, fill ="blue",position="identity")
 
 #histogram over 3 years staff
@@ -34,62 +34,62 @@ low_end_rest <- sample_resturants[sample_resturants$Price_Point_Target == "Low-e
 
 # 3 year profit
 ggplot(data = low_end_rest) +
-  geom_histogram(aes(x = Profit_052019_102019, y=(..count..)/sum(..count..)), 
+  geom_histogram(aes(x = Profit_052019_062019, y=(..count..)/sum(..count..)), 
                  alpha=0.3, fill ="red",position="identity") +
-  geom_histogram(aes(x = Profit_052020_102020, y=(..count..)/sum(..count..)), 
+  geom_histogram(aes(x = Profit_052020_062020, y=(..count..)/sum(..count..)), 
                  alpha=0.3,, fill ="green",position="identity") +
-  geom_histogram(aes(x = Profit_052021_102021, y=(..count..)/sum(..count..)), 
+  geom_histogram(aes(x = Profit_052021_062021, y=(..count..)/sum(..count..)), 
                  alpha=0.7,, fill ="blue",position="identity")
 # 3 year income
 ggplot(data = low_end_rest) +
-  geom_histogram(aes(x = Income_052019_102019, y=(..count..)/sum(..count..)), 
+  geom_histogram(aes(x = Income_052019_062019, y=(..count..)/sum(..count..)), 
                  alpha=0.3, fill ="red",position="identity") +
-  geom_histogram(aes(x = Income_052020_102020, y=(..count..)/sum(..count..)), 
+  geom_histogram(aes(x = Income_052020_062020, y=(..count..)/sum(..count..)), 
                  alpha=0.3,, fill ="green",position="identity") +
-  geom_histogram(aes(x = Income_052021_102021, y=(..count..)/sum(..count..)), 
+  geom_histogram(aes(x = Income_052021_062021, y=(..count..)/sum(..count..)), 
                  alpha=0.7,, fill ="blue",position="identity")
 
-min(low_end_rest$Income_052021_102021) #225666
-mean(low_end_rest$Income_052021_102021) #1435222
+min(low_end_rest$Income_052021_062021) #225666
+mean(low_end_rest$Income_052021_062021) #1435222
 
 #Average Resturants
 average_rest <- sample_resturants[sample_resturants$Price_Point_Target == "Average", ]
 
 #3 years profit
 ggplot(data = average_rest) +
-  geom_histogram(aes(x = Profit_052019_102019, y=(..count..)/sum(..count..)), 
+  geom_histogram(aes(x = Profit_052019_062019, y=(..count..)/sum(..count..)), 
                  alpha=0.3, fill ="red",position="identity") +
-  geom_histogram(aes(x = Profit_052020_102020, y=(..count..)/sum(..count..)), 
+  geom_histogram(aes(x = Profit_052020_062020, y=(..count..)/sum(..count..)), 
                  alpha=0.3,, fill ="green",position="identity") +
-  geom_histogram(aes(x = Profit_052021_102021, y=(..count..)/sum(..count..)), 
+  geom_histogram(aes(x = Profit_052021_062021, y=(..count..)/sum(..count..)), 
                  alpha=0.7,, fill ="blue",position="identity")
 
 
-plot_profit_average <- data.frame(Profit_052019_102019 = average_rest$Profit_052019_102019, 
-                                  Profit_052020_102020 = average_rest$Profit_052020_102020, 
-                                  Profit_052021_102021 = average_rest$Profit_052021_102021)
+plot_profit_average <- data.frame(Profit_052019_062019 = average_rest$Profit_052019_062019, 
+                                  Profit_052020_062020 = average_rest$Profit_052020_062020, 
+                                  Profit_052021_062021 = average_rest$Profit_052021_062021)
 plot_profit_average <- gather(plot_profit_average)
 
 
 ggplot(plot_profit_average, aes(x = value, fill = key)) +
   geom_density(kernel = "gaussian", alpha = 0.1)
 
-min(average_rest$Profit_052021_102021) #225666
-mean(average_rest$Profit_052021_102021) #1435222
-max(average_rest$Profit_052021_102021)
+min(average_rest$Profit_052021_062021) #225666
+mean(average_rest$Profit_052021_062021) #1435222
+max(average_rest$Profit_052021_062021)
 
 #3 years income
 ggplot(data = average_rest) +
-  geom_histogram(aes(x = Income_052019_102019, y=(..count..)/sum(..count..)), 
+  geom_histogram(aes(x = Income_052019_062019, y=(..count..)/sum(..count..)), 
                  alpha=0.3, fill ="red",position="identity") +
-  geom_histogram(aes(x = Income_052020_102020, y=(..count..)/sum(..count..)), 
+  geom_histogram(aes(x = Income_052020_062020, y=(..count..)/sum(..count..)), 
                  alpha=0.3,, fill ="green",position="identity") +
-  geom_histogram(aes(x = Income_052021_102021, y=(..count..)/sum(..count..)), 
+  geom_histogram(aes(x = Income_052021_062021, y=(..count..)/sum(..count..)), 
                  alpha=0.7,, fill ="blue",position="identity")
 
 
-plot_income_average <- data.frame(Income_052019_102019 = average_rest$Income_052019_102019, Income_052020_102020 = average_rest$Income_052020_102020, 
-                                   Income_052021_102021 = average_rest$Income_052021_102021)
+plot_income_average <- data.frame(Income_052019_062019 = average_rest$Income_052019_062019, Income_052020_062020 = average_rest$Income_052020_062020, 
+                                   Income_052021_062021 = average_rest$Income_052021_062021)
 plot_income_average <- gather(plot_income_average)
 
 
@@ -97,24 +97,24 @@ ggplot(plot_income_average, aes(x = value, fill = key)) +
   geom_density(kernel = "gaussian", alpha = 0.1)
 
 
-min(average_rest$Income_052021_102021) #225666
-mean(average_rest$Income_052021_102021) #1435222
+min(average_rest$Income_052021_062021) #225666
+mean(average_rest$Income_052021_062021) #1435222
 
 
 high_end_rest <- sample_resturants[sample_resturants$Price_Point_Target == "High-end", ]
 #3 years profit
 ggplot(data = high_end_rest) +
-  geom_histogram(aes(x = Profit_052019_102019, y=(..count..)/sum(..count..)), 
+  geom_histogram(aes(x = Profit_052019_062019, y=(..count..)/sum(..count..)), 
                  alpha=0.3, fill ="red",position="identity") +
-  geom_histogram(aes(x = Profit_052020_102020, y=(..count..)/sum(..count..)), 
+  geom_histogram(aes(x = Profit_052020_062020, y=(..count..)/sum(..count..)), 
                  alpha=0.3,, fill ="green",position="identity") +
-  geom_histogram(aes(x = Profit_052021_102021, y=(..count..)/sum(..count..)), 
+  geom_histogram(aes(x = Profit_052021_062021, y=(..count..)/sum(..count..)), 
                  alpha=0.7,, fill ="blue",position="identity")
 
 
-plot_profit_high_end <- data.frame(Profit_052019_102019 = high_end_rest$Profit_052019_102019, 
-                                   Profit_052020_102020 = high_end_rest$Profit_052020_102020, 
-                                   Profit_052021_102021 = high_end_rest$Profit_052021_102021)
+plot_profit_high_end <- data.frame(Profit_052019_062019 = high_end_rest$Profit_052019_062019, 
+                                   Profit_052020_062020 = high_end_rest$Profit_052020_062020, 
+                                   Profit_052021_062021 = high_end_rest$Profit_052021_062021)
 plot_profit_high_end <- gather(plot_profit_high_end)
 
 
@@ -124,16 +124,16 @@ ggplot(plot_profit_high_end, aes(x = value, fill = key)) +
 
 #3 years income
 ggplot(data = high_end_rest) +
-  geom_histogram(aes(x = Income_052019_102019, y=(..count..)/sum(..count..)), 
+  geom_histogram(aes(x = Income_052019_062019, y=(..count..)/sum(..count..)), 
                  alpha=0.3, fill ="red",position="identity") +
-  geom_histogram(aes(x = Income_052020_102020, y=(..count..)/sum(..count..)), 
+  geom_histogram(aes(x = Income_052020_062020, y=(..count..)/sum(..count..)), 
                  alpha=0.3,, fill ="green",position="identity") +
-  geom_histogram(aes(x = Income_052021_102021, y=(..count..)/sum(..count..)), 
+  geom_histogram(aes(x = Income_052021_062021, y=(..count..)/sum(..count..)), 
                  alpha=0.7,, fill ="blue",position="identity")
 
 
-plot_income_high_end <- data.frame(Income_052019_102019 = high_end_rest$Income_052019_102019, Income_052020_102020 = high_end_rest$Income_052020_102020, 
-                                   Income_052021_102021 = high_end_rest$Income_052021_102021)
+plot_income_high_end <- data.frame(Income_052019_062019 = high_end_rest$Income_052019_062019, Income_052020_062020 = high_end_rest$Income_052020_062020, 
+                                   Income_052021_062021 = high_end_rest$Income_052021_062021)
 plot_income_high_end <- gather(plot_profit_high_end)
 
 
@@ -142,7 +142,7 @@ ggplot(plot_income_high_end, aes(x = value, fill = key)) +
   
 
 
-paste("min income is: ", min(low_end_rest$Income_052021_102021)) #225666
+paste("min income is: ", min(low_end_rest$Income_052021_062021)) #225666
 
 write_csv(low_end_rest, "dataset/sample_low_end_resturants.csv")
 write_csv(average_rest, "dataset/sample_average_resturants.csv")
